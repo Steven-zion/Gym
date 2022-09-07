2f1518e4002a39ca23bf8d9f8aedf9dc1d70f7ee
stevenoketch6@gmail.com <stevenoketch6@gmail.com>
2022-09-07 22:07:59 +0300

    logo section commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,15 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = ()=> {
+	const header = document.getElementById('header')
+	// when the scroll is greater than 50 viewport height, add the scroll header class to header tag
+	this.scrollY >= 50
+        ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
